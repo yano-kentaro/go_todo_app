5de47cad0ebafa8558117b289693e2fb40e83834
ken <ynkn0829@gmail.com>
2022-09-12 23:35:59 +0900

    メイン処理テスト

---
+++ main_test.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"net/http"
+	"testing"
+
+	"golang.org/x/sync/errgroup"
+)
+
+func TestRun(t *testing.T) {
+	// run関数を実行してHTTPサーバーを起動
+	ctx, cancel := context.WithCancel(context.Background())
+	eg, ctx := errgroup.WithContext(ctx)
+	eg.Go(func() error {
+		return run(ctx)
+	})
+
+	// リクエストを送信・レスポンスを受信
+	in := "message"
+	rsp, err := http.Get("http://localhost:18080/" + in)
+	if err != nil {
+		t.Errorf("failed to get: %+v", err)
+	}
+	defer rsp.Body.Close()
+
+	// レスポンスボディを取得
+	got, err := io.ReadAll(rsp.Body)
+	if err != nil {
+		t.Fatalf("failed to read: %v", err)
+	}
+
+	// レスポンスを検証
+	want := fmt.Sprintf("Hello, %s!", in)
+	if string(got) != want {
+		t.Errorf("want %q, but got %q:", want, got)
+	}
+
+	// サーバーを終了
+	cancel()
+	if err := eg.Wait(); err != nil {
+		t.Fatal(err)
+	}
+}
