8d3e0ef2aded9edb8a08a8ab5f0fb28f03a01064
ken <ynkn0829@gmail.com>
2022-09-14 22:36:36 +0900

    動的にportを指定

--- main_test.go
+++ main_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"io"
+	"net"
 	"net/http"
 	"testing"
 
@@ -11,15 +12,22 @@ import (
 )
 
 func TestRun(t *testing.T) {
+	l, err := net.Listen("tcp", "localhost:0")
+	if err != nil {
+		t.Fatalf("failed to listen port: %v", err)
+	}
+
 	// run関数を実行してHTTPサーバーを起動
 	ctx, cancel := context.WithCancel(context.Background())
 	eg, ctx := errgroup.WithContext(ctx)
 	eg.Go(func() error {
-		return run(ctx)
+		return run(ctx, l)
 	})
 
 	// リクエストを送信・レスポンスを受信
 	in := "message"
+	url := fmt.Sprintf("http://%s/%s", l.Addr().String(), in)
+	t.Logf("try request to %q", url)
 	rsp, err := http.Get("http://localhost:18080/" + in)
 	if err != nil {
 		t.Errorf("failed to get: %+v", err)
